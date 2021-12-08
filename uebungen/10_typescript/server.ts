0dd20b5443502da0f4e262e559f07c1847269bd5
patrickmetz <patrickmetz@web.de>
2021-12-08 10:57:39 +0100

    Übung 10.3: Deno-Beispiel-HTTP-Server mit Typen ausgezeichnet

--- uebungen/10_typescript/server.ts
+++ uebungen/10_typescript/server.ts
@@ -1,31 +1,21 @@
 // Server code source: https://deno.land/manual@v1.16.4/examples/http_server
 
-// Start listening on port 8080 of localhost.
-const server = Deno.listen({ port: 8080 });
+const server: Deno.Listener = Deno.listen({port: 8080});
 console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
 
-// Connections to the server will be yielded up as an async iterable.
-for await (const conn of server) {
-    // In order to not be blocking, we need to handle each connection individually
-    // without awaiting the function
-    serveHttp(conn);
+for await (const connection: AsyncIterable<Deno.Conn> of server) {
+    serveHttp(connection); // not using await => non blocking
 }
 
-async function serveHttp(conn: Deno.Conn) {
-    // This "upgrades" a network connection into an HTTP connection.
-    const httpConn = Deno.serveHttp(conn);
-    // Each request sent over the HTTP connection will be yielded as an async
-    // iterator from the HTTP connection.
-    for await (const requestEvent of httpConn) {
-        // The native HTTP server uses the web standard `Request` and `Response`
-        // objects.
+async function serveHttp(connection: Deno.Conn) {
+    const httpConnection: Deno.HttpConn = Deno.serveHttp(connection);
+
+    for await (const requestEvent of httpConnection) {
         const body = `Your user-agent is:\n\n${
             requestEvent.request.headers.get(
                 "user-agent",
             ) ?? "Unknown"
         }`;
-        // The requestEvent's `.respondWith()` method is how we send the response
-        // back to the client.
         requestEvent.respondWith(
             new Response(body, {
                 status: 200,
