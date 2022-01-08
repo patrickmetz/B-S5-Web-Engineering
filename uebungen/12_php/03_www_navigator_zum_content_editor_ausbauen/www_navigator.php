8dca66ce4cc70e4e4ed688487d0981bd504b7146
patrickmetz <patrickmetz@web.de>
2022-01-08 21:56:50 +0100

    Übung 12.3: Style ausgelagert. PHP-Dateiendung gesetzt.

---
+++ uebungen/12_php/03_www_navigator_zum_content_editor_ausbauen/www_navigator.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ü 12.3 </title>
+
+    <link href="style.css" rel="stylesheet"/>
+
+    <script src="script.js"></script>
+</head>
+
+<body onload='new WwwNavigator(
+    "header", "aside_left", "article", "aside_right",
+    "./contents.json"
+);'>
+
+<header id="header"></header>
+<aside id="aside_left"></aside>
+<article id="article"></article>
+<aside id="aside_right"></aside>
+<footer id="footer"></footer>
+
+</body>
+</html>
