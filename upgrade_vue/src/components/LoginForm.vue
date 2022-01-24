c70bd9af4e6ee5e2fd41f83ad5172137ea99f830
patrickmetz <patrickmetz@web.de>
2022-01-24 22:23:09 +0100

    eigene Homepage: Vue-Upgrade: dynamisches Login-Formular erstellt

---
+++ upgrade_vue/src/components/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="login">
+    <form @mouseleave="showForm = false">
+      <input @click="showForm = !showForm" v-show="!showForm" type="button"
+             value="Anmelden">
+      <input v-show="showForm" type="text" placeholder="Benutzer">
+      <input v-show="showForm" type="password" placeholder="Passwort">
+      <input v-show="showForm" type="submit" value="abschicken">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  props: {
+    msg: String,
+    showForm: {type: Boolean, default: false}
+  }
+}
+</script>
+
+<style scoped>
+</style>
